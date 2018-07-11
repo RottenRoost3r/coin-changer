e237877148e987228b73f03989ace01cc485762d
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-11 13:25:02 -0400

    pass, 35 returns one dime, one quarter

--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -18,4 +18,8 @@ class TestCoinChanging < Minitest::Test
     def test_assert_that_dime_works
     	assert_equal({:dime=>1}, coins(10))
     end
+
+    def test_assert_that_dime_works_with_quarter
+    	assert_equal({:quarter=>1, :dime=>1}, coins(35))
+    end
 end

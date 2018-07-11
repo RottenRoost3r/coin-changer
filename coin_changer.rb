075b2a9b77a71267f1bd4767da1bafdb68e5e979
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-11 13:19:42 -0400

    pass, returns 1 quarter for 25

--- coin_changer.rb
+++ coin_changer.rb
@@ -2,5 +2,12 @@ def coins(number)
 	coin = {}
 	q_count = 0; d_count = 0; n_count = 0; p_count = 0
 	ammounts = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
+	until number == 0
+		if number >= ammounts[:quarter]
+			number -= ammounts[:quarter]
+			q_count += 1
+			coin.store(:quarter, q_count)
+		end
+	end
 	coin
 end
